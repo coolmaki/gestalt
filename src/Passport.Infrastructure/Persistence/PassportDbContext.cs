afc97a0c1bb70dd571dc667b1cd22c063c710e6b
Maki <makidotnet@gmail.com>
2026-06-28 18:00:52 +1000

    refactor: use PersistenceProviderExtensions.Configure in DbContext and DI

--- src/Passport.Infrastructure/Persistence/PassportDbContext.cs
+++ src/Passport.Infrastructure/Persistence/PassportDbContext.cs
@@ -1,39 +1,30 @@
 using Microsoft.EntityFrameworkCore;
-
 using Passport.Core.Domain.Entities;
 using Passport.Infrastructure.Configuration;
 using Passport.Infrastructure.Extensions;
 
 namespace Passport.Infrastructure.Persistence;
 
-internal sealed class PassportDbContext(
-    DbContextOptions<PassportDbContext> options,
-    InfrastructureConfiguration config)
+internal sealed class PassportDbContext(DbContextOptions<PassportDbContext> options, PersistenceConfiguration config)
     : DbContext(options)
 {
-    private readonly PersistenceConfiguration _config = config.Persistence;
-
     internal DbSet<User> Users => Set<User>();
     internal DbSet<RecoveryCode> RecoveryCodes => Set<RecoveryCode>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        _config.Provider.Configure(
-            configureSqlite: () => ConfigureSqlite(modelBuilder),
-            configurePostgres: () => ConfigurePostgres(modelBuilder));
-    }
-
-    private static void ConfigureSqlite(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfiguration(new Configurations.Sqlite.UserConfiguration());
-        modelBuilder.ApplyConfiguration(new Configurations.Sqlite.PasskeyCredentialConfiguration());
-        modelBuilder.ApplyConfiguration(new Configurations.Sqlite.RecoveryCodeConfiguration());
-    }
-
-    private static void ConfigurePostgres(ModelBuilder modelBuilder)
-    {
-        modelBuilder.ApplyConfiguration(new Configurations.Postgres.UserConfiguration());
-        modelBuilder.ApplyConfiguration(new Configurations.Postgres.PasskeyCredentialConfiguration());
-        modelBuilder.ApplyConfiguration(new Configurations.Postgres.RecoveryCodeConfiguration());
+        config.Provider.Configure(
+            configureSqlite: () =>
+            {
+                modelBuilder.ApplyConfiguration(new Configurations.Sqlite.UserConfiguration());
+                modelBuilder.ApplyConfiguration(new Configurations.Sqlite.PasskeyCredentialConfiguration());
+                modelBuilder.ApplyConfiguration(new Configurations.Sqlite.RecoveryCodeConfiguration());
+            },
+            configurePostgres: () =>
+            {
+                modelBuilder.ApplyConfiguration(new Configurations.Postgres.UserConfiguration());
+                modelBuilder.ApplyConfiguration(new Configurations.Postgres.PasskeyCredentialConfiguration());
+                modelBuilder.ApplyConfiguration(new Configurations.Postgres.RecoveryCodeConfiguration());
+            });
     }
 }
